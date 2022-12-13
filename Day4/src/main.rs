a0f3784c207d7886b427b39db4d8d3d2d3c1d43a
Jonas Johansson <contact@jonasjohansson.dev>
2022-12-13 10:57:33 +0100

    Day 4 done

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -1,3 +1,30 @@
+
 fn main() {
-    println!("Hello, world!");
+    let content = include_str!("../input.txt");//.to_string();
+    let mut pairs = 0;
+    for line in content.lines() {
+        println!("{line}");
+        let (one, two) = line.split_once(',').unwrap();
+        let x: (u32, u32) = one.split_once('-').map(|(x, y)| (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap())).unwrap();
+        let y: (u32, u32) = two.split_once('-').map(|(x, y)| (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap())).unwrap();
+        if overlaps(x, y) || fully_contains(x, y) {
+            pairs += 1;
+        }
+    }
+    
+    println!("pairs: {}", pairs);
+}
+
+fn fully_contains(one: (u32, u32), two: (u32, u32)) -> bool {
+    if one.0 <= two.0 && one.1 >= two.1 || two.0 <= one.0 && two.1 >= one.1 {
+        return true
+    }
+    false
+}
+
+fn overlaps(one: (u32, u32), two: (u32, u32)) -> bool {
+    if (one.0 >= two.0 && one.0 <= two.1) || (two.0 >= one.0 && two.0 <= one.1) {
+        return true
+    }
+    false
 }
